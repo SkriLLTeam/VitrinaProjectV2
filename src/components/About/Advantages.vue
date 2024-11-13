43220a07a0e2bff04d7b18097798f9ce53c6f690
stas <miraykim101@gmail.com>
2024-11-13 18:58:04 +0500

    save

--- src/components/About/Advantages.vue
+++ src/components/About/Advantages.vue
@@ -1,30 +1,42 @@
 <template>
   <section class="about__advantages container">
     <h3 class="about__advantages-title">
-      Ищите <span>новостройку?</span> Тогда вам точно к нам:
+      {{ $t("contents.looking.looking_for") }}
+      <span>{{ $t("contents.looking.new_building") }}</span>
+      {{ $t("contents.looking.come_to_us") }}
     </h3>
-
     <div class="about__advantages-wrapper">
       <div
         class="about__advantages-wrapper-item"
-        v-for="(advantage, index) in advantages"
+        v-for="(item, index) in advantagesList"
         :key="index"
       >
-        <span>{{ advantage }}</span>
+        <span>{{ $t(item.title) }}</span>
       </div>
     </div>
   </section>
 </template>
 
 <script setup>
-import { computed } from "vue";
-import { useI18n } from "vue-i18n";
-const { messages, locale } = useI18n();
-const advantages = computed(() => {
-  const currentLocale = locale.value;
-  const advantage = messages.value[currentLocale]?.contents?.offer_why_us_list;
-  return advantage;
-});
+import { ref } from "vue";
+
+const advantagesList = ref([
+  {
+    title: "contents.offer_why_us_list.free_consultation",
+  },
+  {
+    title: "contents.offer_why_us_list.one_percent_commission",
+  },
+  {
+    title: "contents.offer_why_us_list.time_energy_saving",
+  },
+  {
+    title: "contents.offer_why_us_list.all_developers_in_one_place",
+  },
+  {
+    title: "contents.offer_why_us_list.verified_developers_only",
+  },
+]);
 </script>
 
 <style lang="scss" scoped></style>
