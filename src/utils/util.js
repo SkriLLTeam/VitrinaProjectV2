1ae29500ee121f6ca0dd83d5f4c7180a898a1ce6
stas <miraykim101@gmail.com>
2024-12-09 19:25:28 +0500

    save

--- src/utils/util.js
+++ src/utils/util.js
@@ -6,15 +6,14 @@ export const advertisements = "https://vitrina-admin.uz/api/v1/advertisements/";
 export const categories = "https://vitrina-admin.uz/api/v1/categories/";
 // Районы
 export const districts = "https://vitrina-admin.uz/api/v1/districts/";
-// Модалка 
-export const modalPost = "https://vitrina-admin.uz/api/v1/request/add/";
-// Обратная связь 
+// Модалка
+export const modalPost = "https://vitrina-admin.uz/api/v1/request/add";
+// Обратная связь
 export const consultationPost =
-  "https://vitrina-admin.uz/api/v1/consultation/create/";
+  "https://vitrina-admin.uz/api/v1/consultation/create";
 
 export const postHeaders = {
   "Content-Type": "application/json",
   // "Content-Type": "application/x-www-form-urlencoded",
   // "Content-Type": "multipart/form-data",
 };
-
