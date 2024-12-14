a32d4d0af33a1eb66bb074677c529d924d3a8954
skrill <timurkazah1998@gmail.com>
2024-12-14 12:57:35 +0500

    meta added

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import i18n from "@/plugins/i18n";
 import Home from "../views/Home.vue";
-import { useHead } from "@vueuse/head";
+// import { useHead } from "@vueuse/head";
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
   history: createWebHistory(),
@@ -12,20 +12,20 @@ const router = createRouter({
       component: Home,
       meta: {
         title: "titles.home",
-        metaTags: [
-          {
-            property: 'og:title',
-            content: 'Квартиры в Ташкенте – Аренда и покупка по лучшим ценам'
-          },
-          {
-            property: 'og:description',
-            content: 'Ищете квартиру в Ташкенте? Найдите подходящее жилье для аренды или покупки. Широкий выбор, выгодные условия, удобный поиск.'
-          },
-          {
-            property: 'keywords',
-            content: 'квартиры в Ташкенте, аренда квартиры, аренда квартир в Ташкенте, квартира аренда Ташкент'
-          }
-        ]
+        // metaTags: [
+        //   {
+        //     property: 'og:title',
+        //     content: 'Квартиры в Ташкенте – Аренда и покупка по лучшим ценам'
+        //   },
+        //   {
+        //     property: 'og:description',
+        //     content: 'Ищете квартиру в Ташкенте? Найдите подходящее жилье для аренды или покупки. Широкий выбор, выгодные условия, удобный поиск.'
+        //   },
+        //   {
+        //     property: 'keywords',
+        //     content: 'квартиры в Ташкенте, аренда квартиры, аренда квартир в Ташкенте, квартира аренда Ташкент'
+        //   }
+        // ]
       },
     },
     {
@@ -34,20 +34,20 @@ const router = createRouter({
       component: () => import("@/views/Mortgage.vue"),
       meta: {
         title: "titles.mortgage",
-        metaTags: [
-          {
-            property: 'og:title',
-            content: 'Ипотека в Ташкенте – Доступное жилье с выгодными условиями'
-          },
-          {
-            property: 'og:description',
-            content: 'Подберите удобную ипотеку в Ташкенте. Большой выбор предложений, понятные условия и помощь в оформлении. Сделайте шаг к своему дому уже сегодня!'
-          },
-          {
-            property: 'keywords',
-            content: 'квартиры в ипотеку Ташкент, ипотека в Ташкенте, оформить ипотеку в Ташкенте'
-          }
-        ]
+        // metaTags: [
+        //   {
+        //     property: 'og:title',
+        //     content: 'Ипотека в Ташкенте – Доступное жилье с выгодными условиями'
+        //   },
+        //   {
+        //     property: 'og:description',
+        //     content: 'Подберите удобную ипотеку в Ташкенте. Большой выбор предложений, понятные условия и помощь в оформлении. Сделайте шаг к своему дому уже сегодня!'
+        //   },
+        //   {
+        //     property: 'keywords',
+        //     content: 'квартиры в ипотеку Ташкент, ипотека в Ташкенте, оформить ипотеку в Ташкенте'
+        //   }
+        // ]
       },
     },
     {
@@ -97,8 +97,8 @@ router.afterEach((to) => {
   const titleKey = to.meta.title;
   document.title = titleKey ? i18n.global.t(titleKey) : "Vitrina";
   const { meta } = to
-  useHead({
-    meta: meta.metaTags || []
-  })
+  // useHead({
+  //   meta: meta.metaTags || []
+  // })
 });
 export default router;
